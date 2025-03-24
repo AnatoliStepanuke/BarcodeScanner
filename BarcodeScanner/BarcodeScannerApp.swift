3dd0db32d8da10afc47edb19f23d09dee0ab5530
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2025-03-24 20:35:23 +0300

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Anatoli on 24.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
