82528c31ed2672803c6b2668368422e71e994a4f
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2025-03-25 22:32:54 +0300

    Created ScannerViewController to check barcodes from camera

---
+++ BarcodeScanner/View Controller/ScannerViewController.swift
@@ -0,0 +1,60 @@
+import UIKit
+import AVFoundation
+
+protocol ScannerViewControllerDelegate: AnyObject {
+    func didFind(barcode: String)
+}
+
+final class ScannerViewController: UIViewController {
+    
+    // MARK: - Constants
+    let captureSession = AVCaptureSession()
+    
+    // MARK: - Variables
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    weak var scannerDelegate: ScannerViewControllerDelegate?
+    
+    // MARK: - Init
+    init(scannerDelegate: ScannerViewControllerDelegate) {
+        super.init(nibName: nil, bundle: nil)
+        self.scannerDelegate = scannerDelegate
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    // MARK: - Setups
+    private func setupCaptureSession() {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+            return
+        }
+        
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            print("Failed to create video input: \(error)")
+            return
+        }
+        
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            return
+        }
+        
+        let metaDataOutput = AVCaptureMetadataOutput()
+        
+        if captureSession.canAddOutput(metaDataOutput) {
+            captureSession.addOutput(metaDataOutput)
+            metaDataOutput.setMetadataObjectsDelegate(self, queue: .main)
+            metaDataOutput.metadataObjectTypes = [.ean8, .ean13]
+        } else {
+            return
+        }
+    }
+}
+
+extension ScannerViewController: AVCaptureMetadataOutputObjectsDelegate {
+    
+}
